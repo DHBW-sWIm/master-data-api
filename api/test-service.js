f7abaabada184c0a3f1f68762179cee32a35e1d2
Tillman Galla <Tillman.Galla@sas.com>
2020-02-17 13:16:23 +0100

    added some comments for better understanding of the prototype

--- api/test-service.js
+++ api/test-service.js
@@ -4,6 +4,7 @@ const mariadb = require('mariadb/callback');
 exports.test = function(req, res) {
     //res.status(200).json({ message: 'hooray! welcome to our api! it works' });
 
+    // create a connection object with the nessecary data, to establish a connection to mariadb from moodle
     const conn = mariadb.createConnection({
         host: 'adminer.md.swimdhbw.de', 
         user:'bn_moodle',
@@ -11,6 +12,7 @@ exports.test = function(req, res) {
         database: 'bitnami_moodle'
     });
 
+    // try to connect to the DB
     conn.connect(err => {
         if (err) {
             console.log("not connected due to error: " + err);
--- api/test-user-controller.js
+++ api/test-user-controller.js
@@ -1,6 +1,7 @@
 //const express = require("express");
 const testUserController = require("./test-service");
 
+// calling function in testUser Controller to simulate cascading calls throught th API
 exports.testFunction = function(req, res) {
     testUserController.test(req, res);
 }
