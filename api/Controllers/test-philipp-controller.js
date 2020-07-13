72c6c8b4e49bbd3843d1fdc51f184d2f98e21d71
Philipp Preuss <philipp.preuss@mail.de>
2020-07-13 18:56:40 +0200

    Added Camunda Routes to support more Endpoints for certain processes

--- api/Controllers/test-philipp-controller.js
+++ api/Controllers/test-philipp-controller.js
@@ -3,21 +3,19 @@ const axios = require('axios');
 exports.test = async function test(req) {
 
     const path = req.url.split("/");
-    path.splice(0,2);
+    path.splice(0, 2);
     if (path[0] === "getTasks") {
         const url = "https://camunda.md.swimdhbw.de/engine-rest/task";
         if (path[1] == null) {
             const response = await axios.get(url);
             return response.data;
-        }
-        else {
+        } else {
             const response = await axios.get(url
                 + "?assignee="
                 + path[1]);
             return response.data;
         }
-    }
-    else if (path[0] === "createUser") {
+    } else if (path[0] === "createUser") {
         const newUser =
             {
                 "profile": {
@@ -30,7 +28,33 @@ exports.test = async function test(req) {
                     "password": req.body.credentials.password
                 }
             };
-        const response = await axios.post('https://camunda.md.swimdhbw.de/engine-rest/user/create', newUser);
+        const response = await axios.post("https://camunda.md.swimdhbw.de/engine-rest/user/create", newUser);
+        return response.data;
+    } else if (path[0] === "process-definition") {
+        let url;
+
+        if (path[1] === "key") {
+            url = "https://camunda.md.swimdhbw.de/engine-rest"
+                + "/process-definition/"
+                + path[2]
+                + "/start";
+        } else {
+            url = "https://camunda.md.swimdhbw.de/engine-rest"
+                + "/process-definition/"
+                + path[1]
+                + "/start";
+        }
+
+        const data = {
+            "variables": req.body.variables,
+            "businessKey": req.body.businessKey,
+            "caseInstanceId": req.body.caseInstanceId,
+            "startInstructions": req.body.startInstructions,
+            "skipCustomListeners": req.body.skipCustomListeners,
+            "skipIoMappings": req.body.skipIoMappings,
+            "withVariablesInReturn": req.body.withVariablesInReturn
+        };
+        const response = await axios.post(url, data);
         return response.data;
     }
 };
